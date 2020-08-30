2fd590a794a1bad0f983f5d9b6e3dbb222b1c4ad
Luís Gustavo <luisgsilva270@gmail.com>
2020-08-30 12:04:18 -0300

    operadores.dart file added

---
+++ fundamentos/operadores.dart
@@ -0,0 +1,12 @@
+main(){
+
+    bool ehFragil = true;
+    bool ehCaro = true;
+
+    print(ehFragil && ehCaro);
+    print(ehFragil || ehCaro);
+    print(ehFragil ^ ehCaro);
+    print(!ehFragil);
+    print(!!ehCaro);
+
+}
